291a19d929d69f3aabb450d0d529afdb9611185c
NagasawaYusei <curga3013@gmail.com>
2022-05-14 01:18:09 +0900

    Animパラメーター追加

--- Assets/0.All/Script/Test/First.cs
+++ Assets/0.All/Script/Test/First.cs
@@ -4,11 +4,7 @@ public class First : MonoBehaviour
 {
     void Awake()
     {
-        if(!GameManager.Instance.First)
-        {
-            GameManager.Instance.PosNum = 1;
-            GameManager.Instance.First = true;
-        }
+  
     }
 
     //private void Update()
