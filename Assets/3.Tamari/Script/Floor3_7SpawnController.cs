929462d5d9f643b03c13ff09c8196b377bbdde0b
DESKTOP-A427LQ4\vantan <shutaro0415@outlook.jp>
2022-05-23 09:20:02 +0900

    Fllor3-7ベッドギミック完成

--- Assets/3.Tamari/Script/Floor3_7SpawnController.cs
+++ Assets/3.Tamari/Script/Floor3_7SpawnController.cs
@@ -1,25 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Floor3_7SpawnController : MonoBehaviour
 {
     [Header("生成される敵"), Tooltip("生成される敵"), SerializeField] GameObject _enemy;
     [Header("生成開始のフラグ"), Tooltip("生成開始のフラグ")] bool _isSpawned;
+    [SerializeField,Tooltip("エネミーの出現する場所")] Transform[] _spawnPos = new Transform[4];
+
+    Light2D _light = default;
 
     // Start is called before the first frame update
-    void Start()
-    {
-        AppearEnemys();
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     void AppearEnemys()
     {
-        GameObject.Instantiate(_enemy, transform.position, Quaternion.identity);
+        if (_isSpawned) return;
+        foreach(var pos in _spawnPos)
+        {
+            GameObject.Instantiate(_enemy, pos.position, Quaternion.identity);
+            _isSpawned = true;
+        }   
+    }
+    //void ChangeLight2D()
+    //{
+    //    if (_isSpawned) return;
+        
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            AppearEnemys();
+        }
     }
 }
