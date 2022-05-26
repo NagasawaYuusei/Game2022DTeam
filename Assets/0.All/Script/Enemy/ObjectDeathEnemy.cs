903cd68be4f3314ee8a78ee4049efc0e58745ab3
NagasawaYusei <curga3013@gmail.com>
2022-05-26 22:42:54 +0900

    Enemy途中

--- Assets/0.All/Script/Enemy/ObjectDeathEnemy.cs
+++ Assets/0.All/Script/Enemy/ObjectDeathEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObjectEnemyDeath : MonoBehaviour
+public class ObjectDeathEnemy : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
