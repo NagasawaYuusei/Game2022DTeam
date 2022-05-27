7fcef3f85e4b613be7aee917f04eacab2ba5cd10
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-05-27 22:03:59 +0900

    [Anim][Scritp][scene]改良

--- Assets/2.KenChan/Scripts/EnemySpown.cs
+++ Assets/2.KenChan/Scripts/EnemySpown.cs
@@ -7,7 +7,7 @@ public class EnemySpown : MonoBehaviour
     [Header("スポーン")]
     [SerializeField, Tooltip("スポーンする敵")] GameObject _enemy;
     [Tooltip("スポーンする場所")] Transform _pos;
-
+    [Tooltip("発動したかの判定")] bool _check;
     private void Start()
     {
         _pos = this.gameObject.GetComponent<Transform>();
@@ -19,9 +19,10 @@ public class EnemySpown : MonoBehaviour
     /// <param name="collision"></param>
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player") && !_check)
         {
             Instantiate(_enemy, _pos.position, Quaternion.identity);
+            _check = true;
         }
     }
 }
