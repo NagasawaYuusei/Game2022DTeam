88ca7c579e527f39622edac4c326d38dfe25079d
DESKTOP-A427LQ4\vantan <shutaro0415@outlook.jp>
2022-05-28 17:58:12 +0900

    サイレン部屋改良　

--- Assets/3.Tamari/Script/Floor3_7SpawnController.cs
+++ Assets/3.Tamari/Script/Floor3_7SpawnController.cs
@@ -8,11 +8,8 @@ public class Floor3_7SpawnController : MonoBehaviour
     [Header("生成される敵"), Tooltip("生成される敵"), SerializeField] GameObject _enemy;
     [Header("生成開始のフラグ"), Tooltip("生成開始のフラグ")] bool _isSpawned;
     [SerializeField,Tooltip("エネミーの出現する場所")] Transform[] _spawnPos = new Transform[4];
+    [SerializeField, Tooltip("ライトの色")] Light2D _light;
 
-    Light2D _light = default;
-
-    // Start is called before the first frame update
-    
     void AppearEnemys()
     {
         if (_isSpawned) return;
@@ -22,17 +19,12 @@ public class Floor3_7SpawnController : MonoBehaviour
             _isSpawned = true;
         }   
     }
-    //void ChangeLight2D()
-    //{
-    //    if (_isSpawned) return;
-        
-    //}
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Player"))
         {
             AppearEnemys();
+            _light.color = Color.red;
         }
     }
 }
